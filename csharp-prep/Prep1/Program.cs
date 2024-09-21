517f4da91f11d4ea8e9130b58df124b940705439
mal20012 <mal20012@byui.edu>
2024-09-21 21:07:57 +0200

    MAde the needed changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your firstname?");
+                string firstname = Console.ReadLine();
+
+        Console.Write("What is your second name?");
+                string name = Console.ReadLine();
+        
+        Console.WriteLine("Your name is, " + firstname + name);
     }
 }
