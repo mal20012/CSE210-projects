b54fddab557cb7782d93b72fb2a75da14745c089
mal20012 <mal20012@byui.edu>
2024-09-22 06:30:43 +0200

    Did the requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+        int nombre = -1;
+        while (nombre != 0) {
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            string answer = Console.ReadLine();
+            nombre = int.Parse(answer);
+
+            if (nombre != 0){
+                numbers.Add(nombre);
+
+            }
+        }   
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("The sum is: " + sum);
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine("The average is: " + average);
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.Write("The max is: "+ max);
+    }   
 }
