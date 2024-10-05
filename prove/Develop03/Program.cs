e564d1ca2760688cf82209000dc783b316065b31
mal20012 <mal20012@byui.edu>
2024-10-05 23:17:50 +0200

    Completed the Develop03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        Scripture scripture = new Scripture("3 Nephi ", 5, 13, "Behold, I am a disciple of Jesus Christ, the Son of God. I have been called of him to declare his word among his people, that they might have everlasting life. ");
+        Console.Clear();
+        scripture.Display(); 
+        Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            break;
+
+            if (!scripture.HideRandomWord())
+            {
+                Console.WriteLine("\nYou got this, you memorized the scripture!!");
+                break;
+                }
+                
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
-}
+    
